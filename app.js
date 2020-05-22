e3876f52e0860b72c9624bf183d2e33319306ca2
DimOnChik88 <piskundima88@gmail.com>
2020-05-22 09:14:57 +0300

    Server side for real estate

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const {resolve} = require('path');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+const db = require('./dataBase').getInstance();
+db.setModels();
+
+app.use(fileUpload({}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(resolve(__dirname, 'public')));
+global.appRoot = __dirname;
+
+const {userRouter, houseRouter, authRouter, adminRouter} = require('./routes');
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next()
+});
+
+app.use('/users', userRouter);
+app.use('/houses', houseRouter);
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
+
+app.all('*', (req, res) => {
+    res.status(404).end();
+});
+
+
+app.listen(3000, (err) => {
+    if (err) console.log(err);
+    else console.log('Listen 3000 ....')
+});
